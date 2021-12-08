a0d659008a714d2706c8056f6df8d80dfc92f113
Nipun Jain <nipunjain1305@gmail.com>
2021-12-08 10:46:11 +0530

    Update main2.js

--- experiment/simulation/js/main2.js
+++ experiment/simulation/js/main2.js
@@ -50,51 +50,16 @@ var objImage6 = null;
             case 85: //u alphabet key
                 moveu();
                 break;
-            case 65: //a alphabet key
-                left_rotate();
-                 break;
-            case 66: //b alphabet key
-                 right_rotate();
-                  break;
-            case 67: //c alphabet key
-                  up_rotate();
-                   break;
             case 68: //d alphabet key
                 moved();
                 break;
-            case 69: //e alphabet key
-                  down_rotate();
-                   break;
-            case 70: //f alphabet key
-                  up_dot();
-                   break;
-            case 71: //g alphabet key
-                   down_dot();
-                    break;
+           
            
         }
     }
   
     function moveLeft() {
-        objImage.style.left = parseInt(objImage.style.left) - 1 + "px";
-        
-    }
-    function moveUp() {
-        objImage.style.top = parseInt(objImage.style.top) - 0 + "px";
-        
-
-    }
-    function moveRight() {
-        objImage.style.left = parseInt(objImage.style.left) + 1 + "px";
-       
-    }
-    function moveDown() {
-        objImage.style.top = parseInt(objImage.style.top) + 0 + "px";
-        
-    }
-    function movel() {
-        
-        objImage2.style.left = parseInt(objImage2.style.left) - 1 + "px";
+         objImage2.style.left = parseInt(objImage2.style.left) - 1 + "px";
           
     if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-189px")
     {
@@ -135,55 +100,10 @@ var objImage6 = null;
       
       swal("Click on LEVEL SETUP Button");
     }  
-    }
-    function mover() {
-        
-        objImage2.style.left = parseInt(objImage2.style.left) + 1 + "px";
-       
-    if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-189px")
-    {
-        
-        document.getElementById("level_setting").style.display = "inline";
-        document.getElementById("tri_img2").style.display = "none";
-        document.getElementById("autolevel2").style.display = "none";
-        document.getElementById("setup_autotri").style.display = "inline";
-        
-        swal("Click on LEVEL SETUP Button");
-    }
-    else
-    if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-188px")
-    {
-        document.getElementById("level_setting").style.display = "inline";
-        document.getElementById("tri_img2").style.display = "none";
-        document.getElementById("autolevel2").style.display = "none";
-        document.getElementById("setup_autotri").style.display = "inline";
-      
-      swal("Click on LEVEL SETUP Button");
-    }
-    else
-    if(document.getElementById("autolevel2").style.left === "127px" && document.getElementById("autolevel2").style.top === "-190px")
-    {
-        document.getElementById("level_setting").style.display = "inline";
-        document.getElementById("tri_img2").style.display = "none";
-        document.getElementById("autolevel2").style.display = "none";
-        document.getElementById("setup_autotri").style.display = "inline";
-       
-      swal("Click on LEVEL SETUP Button");
-    }
-    if(document.getElementById("autolevel2").style.left === "127px" && document.getElementById("autolevel2").style.top === "-189px")
-    {
-        document.getElementById("level_setting").style.display = "inline";
-        document.getElementById("tri_img2").style.display = "none";
-        document.getElementById("autolevel2").style.display = "none";
-        document.getElementById("setup_autotri").style.display = "inline";
-       
-      swal("Click on LEVEL SETUP Button");
-    }  
         
     }
-    function moveu() {
-        
-        objImage2.style.top = parseInt(objImage2.style.top) - 1 + "px";
+    function moveUp() {
+      objImage2.style.top = parseInt(objImage2.style.top) - 1 + "px";
         
     if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-189px")
     {
@@ -225,10 +145,54 @@ var objImage6 = null;
       swal("Click on LEVEL SETUP Button");
     }  
     
+
     }
-    function moved() {
-        
+    function moveRight() {
+        objImage2.style.left = parseInt(objImage2.style.left) + 1 + "px";
        
+    if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-189px")
+    {
+        
+        document.getElementById("level_setting").style.display = "inline";
+        document.getElementById("tri_img2").style.display = "none";
+        document.getElementById("autolevel2").style.display = "none";
+        document.getElementById("setup_autotri").style.display = "inline";
+        
+        swal("Click on LEVEL SETUP Button");
+    }
+    else
+    if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-188px")
+    {
+        document.getElementById("level_setting").style.display = "inline";
+        document.getElementById("tri_img2").style.display = "none";
+        document.getElementById("autolevel2").style.display = "none";
+        document.getElementById("setup_autotri").style.display = "inline";
+      
+      swal("Click on LEVEL SETUP Button");
+    }
+    else
+    if(document.getElementById("autolevel2").style.left === "127px" && document.getElementById("autolevel2").style.top === "-190px")
+    {
+        document.getElementById("level_setting").style.display = "inline";
+        document.getElementById("tri_img2").style.display = "none";
+        document.getElementById("autolevel2").style.display = "none";
+        document.getElementById("setup_autotri").style.display = "inline";
+       
+      swal("Click on LEVEL SETUP Button");
+    }
+    if(document.getElementById("autolevel2").style.left === "127px" && document.getElementById("autolevel2").style.top === "-189px")
+    {
+        document.getElementById("level_setting").style.display = "inline";
+        document.getElementById("tri_img2").style.display = "none";
+        document.getElementById("autolevel2").style.display = "none";
+        document.getElementById("setup_autotri").style.display = "inline";
+       
+      swal("Click on LEVEL SETUP Button");
+    }  
+       
+    }
+    function moveDown() {
+           
     if(document.getElementById("autolevel2").style.left === "126px" && document.getElementById("autolevel2").style.top === "-189px")
     {
         
@@ -271,12 +235,11 @@ var objImage6 = null;
         
     }
     var j=150;
-    
-    function left_rotate() {
-       
+    function movel() {
+
         objImage4.style.transform="rotateZ(" + j + "deg)";
         objImage5.style.transform="rotateZ(" + j + "deg)";
-        objImage6.style.left = parseInt(objImage6.style.left) - 1 + "px";
+	objImage6.style.left = parseInt(objImage6.style.left) - 1 + "px";
         j=j-1;
         if(document.getElementById("bluedot").style.left === "-1px" && document.getElementById("bluedot").style.top === "7px")
                 {
@@ -288,10 +251,10 @@ var objImage6 = null;
                         swal("Click on NEXt Button");
                 }
     }
-    var i=50;
-    var a=-500;
-    function right_rotate() {
-     
+ 	var i=50;
+ 	var a=-500;
+    function mover() {
+        
         objImage4.style.transform="rotateZ(" + a + "deg)";
         objImage5.style.transform="rotateZ(" + a + "deg)";
         objImage6.style.left = parseInt(objImage6.style.left) + 1 + "px";
@@ -305,12 +268,39 @@ var objImage6 = null;
                     document.getElementById("next_setting").style.display = "inline";
                     swal("Click on NEXt Button");
                 }
-    }
-    function up_rotate() {
-        
-        objImage2.style.transform=" perspective(400px)scaleZ(2)rotateX(40deg)";
         
     }
+	var f=150;
+    function moveu() {
+        
+        objImage3.style.transform="rotateZ(" + f + "deg)";
+        objImage6.style.top = parseInt(objImage6.style.top) - 1 + "px";
+        f=f-1;
+        if(document.getElementById("bluedot").style.left === "-1px" && document.getElementById("bluedot").style.top === "7px")
+                {
+                    document.getElementById("setup").style.display = "none";
+                    document.getElementById("setup_dot").style.display = "inline";
+                    document.getElementById("next_setting").style.display = "inline";
+                    swal("Click on NEXt Button");
+                }
+    }
+	g=-500;
+    function moved() {
+        objImage3.style.transform="rotateZ(" + a + "deg)";
+        objImage6.style.top = parseInt(objImage6.style.top) + 1 + "px";
+        a=a+1;
+        if(document.getElementById("bluedot").style.left === "-1px" && document.getElementById("bluedot").style.top === "7px")
+        {
+                document.getElementById("setup").style.display = "none";
+                document.getElementById("setup_dot").style.display = "inline";
+                document.getElementById("next_setting").style.display = "inline";
+                swal("Click on NEXt Button");
+        }
+    
+    }
+    
+    
+
 
     function setup()
     {   if(document.getElementById("setup").style.display === "none")
@@ -325,32 +315,8 @@ var objImage6 = null;
             document.getElementById("cap").style.display = "none";
         }
     }
-    var f=150;
-    function up_dot(){
-        objImage3.style.transform="rotateZ(" + f + "deg)";
-        objImage6.style.top = parseInt(objImage6.style.top) - 1 + "px";
-        f=f-1;
-        if(document.getElementById("bluedot").style.left === "-1px" && document.getElementById("bluedot").style.top === "7px")
-                {
-                    document.getElementById("setup").style.display = "none";
-                    document.getElementById("setup_dot").style.display = "inline";
-                    document.getElementById("next_setting").style.display = "inline";
-                    swal("Click on NEXt Button");
-                }
-    }
-    g=-500;
-    function down_dot(){
-        objImage3.style.transform="rotateZ(" + a + "deg)";
-        objImage6.style.top = parseInt(objImage6.style.top) + 1 + "px";
-        a=a+1;
-        if(document.getElementById("bluedot").style.left === "-1px" && document.getElementById("bluedot").style.top === "7px")
-        {
-                document.getElementById("setup").style.display = "none";
-                document.getElementById("setup_dot").style.display = "inline";
-                document.getElementById("next_setting").style.display = "inline";
-                swal("Click on NEXt Button");
-        }
-    }
+    
+ 
     window.onload = init;
   
     function msg1()
